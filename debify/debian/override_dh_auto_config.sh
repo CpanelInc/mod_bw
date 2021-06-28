7937497cbdca0b78fe13879de201a18ac4a89bf1
Julian Brown <julian.brown@cpanel.net>
2021-06-28 13:07:09 -0500

    ZC-8700: Rename the tarball
    The tarball needed to be renamed to build on ubuntu.

---
+++ debify/debian/override_dh_auto_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source debian/vars.sh
+
+set -x
+
+# pulled from apr-util
+mkdir -p config
+cp $ea_apr_config config/apr-1-config
+cp $ea_apr_config config/apr-config
+cp /usr/share/pkgconfig/ea-apr16-1.pc config/apr-1.pc
+cp /usr/share/pkgconfig/ea-apr16-util-1.pc config/apr-util-1.pc
+cp /usr/share/pkgconfig/ea-apr16-1.pc config
+cp /usr/share/pkgconfig/ea-apr16-util-1.pc config
+
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:`pwd`/config"
+touch configure
+
+$_httpd_apxs -c mod_bw.c
+mv .libs/$upstream_name.so .
+strip -g $upstream_name.so
